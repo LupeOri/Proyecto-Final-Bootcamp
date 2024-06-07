d44ad46f7b8c7996efef0d045d0ddd8c5d7fbf75
Mathias Bleuzé <mtbleuz@gmail.com>
2024-06-07 18:51:30 +0200

    creating nac component

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: "", component: HomeComponent
+},
+// {
+//   path: "experiencias", component:
+// },
+// {
+//   path: "contact", component:
+// },
+// {
+//   path: "404", component:
+// },
+{
+  path: "**", redirectTo: ""
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
