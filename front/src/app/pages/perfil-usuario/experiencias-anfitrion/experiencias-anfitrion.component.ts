2d2ec952b3668911d550ec477878d3cdbadf78e7
lupeori <lupe.oriol@gmail.com>
2024-06-13 08:15:39 +0200

    creating experiencias-anfitrion

--- front/src/app/pages/perfil-usuario/experiencias-anfitrion/experiencias-anfitrion.component.ts
+++ front/src/app/pages/perfil-usuario/experiencias-anfitrion/experiencias-anfitrion.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Experiencia } from 'src/app/models/experiencia.model';
+import { LocalLeisureService } from 'src/app/services/local-leisure.service';
+
 
 @Component({
   selector: 'app-experiencias-anfitrion',
   templateUrl: './experiencias-anfitrion.component.html',
   styleUrls: ['./experiencias-anfitrion.component.css']
 })
-export class ExperienciasAnfitrionComponent {
+export class ExperienciasAnfitrionComponent implements OnInit{
 
+  experienciasForm!: FormGroup;
+  nuevaExperiencia: Experiencia = {
+    _id: '',
+    titulo: '',
+    descripcion: '',
+    ubicacion: '',
+    precio: '',
+    duracion: '',
+    categoria: 'otras categorias',
+    usuario: { id: '', tipo: 'anfitrion', nombre: '', email: '', password: '' }, // Usuario por defecto
+    valoraciones: [],
+    imagenes: '',
+    nombre: ''
+  };
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private experienciaService: LocalLeisureService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.experienciasForm = this.formBuilder.group({
+      titulo: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+      duracion: ['', Validators.required],
+      categoria: ['', Validators.required],
+      usuario: this.formBuilder.group({
+        id: [''],
+        tipo: ['anfitrion'],
+        nombre: [''],
+        email: [''],
+        password: ['']
+      }),
+      valoraciones: [''],
+      imagenes: ['', Validators.required],
+      nombre: ['']
+    });
+
+    this.experienciasForm.valueChanges.subscribe((changes) => {
+      this.nuevaExperiencia = { ...this.nuevaExperiencia, ...changes };
+    });
+  }
+
+  onSubmit() {
+    if (this.experienciasForm.valid) {
+      const newExperiencia = this.experienciasForm.value;
+      this.experienciaService.postExperiencia(newExperiencia).subscribe(() => {
+        alert('Experiencia creada');
+        this.router.navigate(['/experiencias']);
+      });
+    }
+  }
 }
--- front/src/app/services/local-leisure.service.ts
+++ front/src/app/services/local-leisure.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { HttpHeaders } from '@angular/common/http';
+import { Experiencia } from '../models/experiencia.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class LocalLeisureService {
 
   private baseUrl: string = "http://localhost:5000/";
 
-  constructor(private http: HttpClient, private authService: AuthService) {} // Inyecta AuthService
+  constructor(private http: HttpClient, private authService: AuthService) {} 
 
   getExperiencias() {
     return this.http.get(`${this.baseUrl}experiencias`);
@@ -24,5 +25,9 @@ export class LocalLeisureService {
     const token = this.authService.getToken(); 
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get(`${this.baseUrl}myreservas`, { headers });
+  }
+
+  postExperiencia(newExperiencia: Experiencia) {
+    return this.http.post(`${this.baseUrl}experiencias`, newExperiencia);
   }
 }
